744c0991a8bd5c3dd8e74a6bee7a8715910d0d95
krzys-h <krzys_h@interia.pl>
2025-08-01 21:53:01 +0200

    Replace the polling REST api with WebSockets

--- src/bin/www_test/server.rs
+++ src/bin/www_test/server.rs
@@ -1,21 +1,23 @@
-use defmt::info;
+use defmt::{error, info};
+use embassy_futures::select::{self, Either4};
 use embassy_time::Duration;
+use serde::{Deserialize, Serialize};
+use serde_json;
 use picoserve::{
     make_static,
-    response::{ErrorWithStatusCode, File, IntoResponse},
-    routing::{self, get, parse_path_segment, PathRouter},
+    response::{ws, File},
+    routing::{self, get, PathRouter},
     AppBuilder, AppRouter, Router,
 };
-use serde::Serialize;
 extern crate alloc;
-use mainboard::board::POWER_STATE;
-use crate::{server::alloc::string::ToString, simple_output::set_state};
+use mainboard::{board::POWER_STATE, power::PowerControllerStats};
+use crate::simple_output::{set_state, watch_output};
 use alloc::string::String;
 
 use crate::wifi::WifiResources;
 use crate::{
     html::INDEX_HTML,
-    simple_output::{get_output_state, OutputID},
+    simple_output::OutputID,
 };
 use bq24296m;
 use mainboard::board::POWER_CONTROL;
@@ -25,20 +27,13 @@ use mainboard::tasks::{PowerRequest, PowerResponse};
 const WEB_TASK_POOL_SIZE: usize = 8;
 
 #[derive(Serialize)]
-struct APIResponse {
-    ok: bool,
-}
-
-#[derive(Serialize)]
 struct PinStatesResponse<'a> {
-    ok: bool,
-    pin0_state: Option<&'a str>,
-    pin1_state: Option<&'a str>,
+    pin_number: u8,
+    state: &'a str,
 }
 
 #[derive(Serialize)]
 struct PowerStatsResponse<'a> {
-    ok: bool,
     // Status register decoded values
     vbus_status: &'a str,
     charge_status: &'a str,
@@ -68,160 +63,204 @@ impl AppBuilder for AppProps {
         Router::new()
             .route("/", routing::get_service(File::html(INDEX_HTML)))
             .route(
-                (
-                    "/api/digital",
-                    parse_path_segment::<u8>(),
-                    parse_path_segment::<u8>(),
-                ),
-                get(digital_handler),
+                "/ws",
+                get(|upgrade: picoserve::response::WebSocketUpgrade| {
+                    upgrade.on_upgrade(WebsocketHandler)
+                }),
             )
-            .route("/api/power/stats", get(power_stats_handler))
-            .route(
-                ("/api/power/boost", parse_path_segment::<bool>()),
-                get(power_boost_handler),
-            )
-            .route("/api/pins/states", get(pin_states_handler))
     }
 }
 
-#[derive(Debug, thiserror::Error, ErrorWithStatusCode)]
-#[status_code(BAD_REQUEST)]
-enum BadRequest {
-    #[error("Bad: {0}")]
-    Bad(String),
+fn format_power_stats_response<'a>(stats: PowerControllerStats) -> PowerStatsResponse<'a> {
+    // Use helper functions to decode register values
+    let status = &stats.charger_status;
+    let faults = &stats.charger_faults;
+
+    info!("Power stats: retrieved successfully");
+
+    // Convert enum values to static string slices to avoid lifetime issues
+    let vbus_status = match status.get_vbus_status() {
+        bq24296m::VbusStatus::Unknown => "Unknown",
+        bq24296m::VbusStatus::UsbHost => "USB Host",
+        bq24296m::VbusStatus::AdapterPort => "Adapter Port",
+        bq24296m::VbusStatus::Otg => "OTG",
+    };
+
+    let charge_status = match status.get_charge_status() {
+        bq24296m::ChargeStatus::NotCharging => "Not Charging",
+        bq24296m::ChargeStatus::PreCharge => "Pre-Charge",
+        bq24296m::ChargeStatus::FastCharging => "Fast Charging",
+        bq24296m::ChargeStatus::ChargeDone => "Charge Done",
+    };
+
+    let charge_fault_status = match faults.get_charge_fault_status() {
+        bq24296m::ChargeFaultStatus::Normal => "Normal",
+        bq24296m::ChargeFaultStatus::InputFault => "Input Fault",
+        bq24296m::ChargeFaultStatus::ThermalShutdown => "Thermal Shutdown",
+        bq24296m::ChargeFaultStatus::ChargeTimerExpired => "Charge Timer Expired",
+    };
+
+    let ntc_fault_status = match faults.get_ntc_fault_status() {
+        bq24296m::NtcFaultStatus::Normal => "Normal",
+        bq24296m::NtcFaultStatus::Cold => "Cold",
+        bq24296m::NtcFaultStatus::Hot => "Hot",
+        bq24296m::NtcFaultStatus::ColdAndHot => "Cold and Hot",
+    };
+
+    PowerStatsResponse {
+        // Status register decoded values
+        vbus_status,
+        charge_status,
+        dpm_active: status.is_dpm_active(),
+        power_good: status.is_power_good(),
+        thermal_regulation_active: status.is_thermal_regulation_active(),
+        vsys_regulation_active: status.is_vsys_regulation_active(),
+        // Fault register decoded values
+        watchdog_fault: faults.is_watchdog_fault(),
+        otg_fault: faults.is_otg_fault(),
+        charge_fault_status,
+        battery_fault: faults.is_battery_fault(),
+        ntc_fault_status,
+        ntc_cold_fault: faults.is_ntc_cold_fault(),
+        ntc_hot_fault: faults.is_ntc_hot_fault(),
+        // Boost converter state
+        boost_converter_enabled: stats.boost_enabled,
+    }
 }
 
-// Handler for digital I/O API
-async fn digital_handler((id, value): (u8, u8)) -> impl IntoResponse {
-    info!("Setting digital pin {} to {}", id, value);
-    let pin: OutputID = match id {
-        0 => OutputID::Output1,
-        1 => OutputID::Output2,
-        _ => return Err(BadRequest::Bad("Invalid pin".to_string())),
-    };
-
-    let value: bool = match value {
-        0 | 1 => value != 0,
-        _ => return Err(BadRequest::Bad("Invalid value".to_string())),
-    };
-    set_state(pin, value).await;
-    // Here you'd implement actual pin control
-    // For now, just return a success message
-    Ok(picoserve::response::Json(APIResponse { ok: true }))
+#[derive(Debug, Deserialize)]
+#[serde(tag = "type")]
+enum WebSocketCommand {
+    #[serde(rename = "digital")]
+    Digital { id: u8, value: u8 },
+    #[serde(rename = "power")]
+    Power { action: String, value: bool },
 }
 
-/// Handler for boost converter control
-async fn power_boost_handler(enable: bool) -> impl IntoResponse {
-    info!(
-        "Setting boost converter to: {}",
-        if enable { "enabled" } else { "disabled" }
-    );
-
-    // Send command to power controller
-    let response = match POWER_CONTROL
-        .transact(PowerRequest::EnableBoostConverter(enable))
-        .await
-    {
-        PowerResponse::Ok => {
-            info!("Boost converter set successfully");
-            APIResponse { ok: true }
-        }
-        PowerResponse::Err(_) => {
-            info!("Failed to set boost converter state");
-            return Err(BadRequest::Bad("Failed to set boost converter".to_string()));
-        }
-    };
-
-    Ok(picoserve::response::Json(response))
+#[derive(Serialize)]
+#[serde(tag = "type")]
+enum OutgoingMessage<'a> {
+    #[serde(rename = "power_stats")]
+    PowerStats(PowerStatsResponse<'a>),
+    #[serde(rename = "pin_state")]
+    PinState(PinStatesResponse<'a>),
 }
 
-/// Handler for getting pin states
-async fn pin_states_handler() -> impl IntoResponse {
-    info!("Getting pin states");
+struct WebsocketHandler;
 
-    // Get pin states from simple_output module
-    let pin0_state = get_output_state(OutputID::Output1).map(|state| state.to_str());
-    let pin1_state = get_output_state(OutputID::Output2).map(|state| state.to_str());
+impl ws::WebSocketCallback for WebsocketHandler {
+    async fn run<R: embedded_io_async::Read, W: embedded_io_async::Write<Error = R::Error>>(
+        self,
+        mut rx: ws::SocketRx<R>,
+        mut tx: ws::SocketTx<W>,
+    ) -> Result<(), W::Error> {
+        let mut buffer = [0; 1024];
 
-    // Create and return the response
-    let response = PinStatesResponse {
-        ok: true,
-        pin0_state,
-        pin1_state,
-    };
+        let Some(mut power_state_receiver) = POWER_STATE.receiver() else {
+            error!("Failed to get power state receiver");
+            let _ = tx.close(Some((1011, "Failed to get power state receiver"))).await;
+            return Ok(());
+        };
+        let Some(mut out1_receiver) = watch_output(OutputID::Output1) else {
+            error!("Failed to watch output 1");
+            let _ = tx.close(Some((1011, "Failed to watch output 1"))).await;
+            return Ok(());
+        };
+        let Some(mut out2_receiver) = watch_output(OutputID::Output2) else {
+            error!("Failed to watch output 2");
+            let _ = tx.close(Some((1011, "Failed to watch output 2"))).await;
+            return Ok(());
+        };
 
-    picoserve::response::Json(response)
-}
+        let close_reason = loop {
+            match select::select4(rx.next_message(&mut buffer), power_state_receiver.changed(), out1_receiver.changed(), out2_receiver.changed()).await {
+                Either4::First(x) => match x {
+                    Ok(ws::Message::Text(data)) => {
+                        // Try to parse the incoming message as a command
+                        if let Ok(command) = serde_json::from_str::<WebSocketCommand>(data) {
+                            match command {
+                                WebSocketCommand::Digital { id, value } => {
+                                    let _ = set_state(match id {
+                                        0 => OutputID::Output1,
+                                        1 => OutputID::Output2,
+                                        _ => {
+                                            error!("Invalid output ID: {}", id);
+                                            continue;
+                                        }
+                                    }, value != 0).await;
+                                }
+                                WebSocketCommand::Power { action, value } => match action.as_str() {
+                                    "boost" => {
+                                        info!(
+                                            "Setting boost converter to: {}",
+                                            if value { "enabled" } else { "disabled" }
+                                        );
+                                    
+                                        // Send command to power controller
+                                        match POWER_CONTROL
+                                            .transact(PowerRequest::EnableBoostConverter(value))
+                                            .await
+                                        {
+                                            PowerResponse::Ok => {
+                                                info!("Boost converter set successfully")
+                                            }
+                                            PowerResponse::Err(_) => {
+                                                info!("Failed to set boost converter state")
+                                            }
+                                        };
+                                    }
+                                    _ => {
+                                        error!("Unknown power action")
+                                    }
+                                },
+                            }
+                        }
+                        continue
+                    }
+                    Ok(ws::Message::Binary(_)) => {
+                        break Some((1003, "Binary messages not supported"))
+                    },
+                    Ok(ws::Message::Close(_)) => {
+                        break None;
+                    }
+                    Ok(ws::Message::Ping(data)) => tx.send_pong(data).await,
+                    Ok(ws::Message::Pong(_)) => continue,
+                    Err(err) => {
+                        let code = match err {
+                            ws::ReadMessageError::Io(err) => return Err(err),
+                            ws::ReadMessageError::ReadFrameError(_)
+                            | ws::ReadMessageError::MessageStartsWithContinuation
+                            | ws::ReadMessageError::UnexpectedMessageStart => 1002,
+                            ws::ReadMessageError::ReservedOpcode(_) => 1003,
+                            ws::ReadMessageError::TextIsNotUtf8 => 1007,
+                        };
 
-/// Handler for power controller stats
-async fn power_stats_handler() -> impl IntoResponse {
-    info!("Getting power controller stats");
+                        break Some((code, "Websocket Error"));
+                    }
+                },
+                Either4::Second(power_state) => {
+                    let power_stats_response = format_power_stats_response(power_state);
+                    tx.send_json(OutgoingMessage::PowerStats(power_stats_response)).await
+                }
+                Either4::Third(out1_state) => {
+                    let pin_state_response = PinStatesResponse {
+                        pin_number: 0,
+                        state: out1_state.to_str(),
+                    };
+                    tx.send_json(OutgoingMessage::PinState(pin_state_response)).await
+                }
+                Either4::Fourth(out2_state) => {
+                    let pin_state_response = PinStatesResponse {
+                        pin_number: 1,
+                        state: out2_state.to_str(),
+                    };
+                    tx.send_json(OutgoingMessage::PinState(pin_state_response)).await
+                }
+            }?;
+        };
 
-    let response = match POWER_STATE.try_get() {
-        Some(stats) => {
-            // Use helper functions to decode register values
-            let status = &stats.charger_status;
-            let faults = &stats.charger_faults;
-
-            info!("Power stats: retrieved successfully");
-
-            // Convert enum values to static string slices to avoid lifetime issues
-            let vbus_status = match status.get_vbus_status() {
-                bq24296m::VbusStatus::Unknown => "Unknown",
-                bq24296m::VbusStatus::UsbHost => "USB Host",
-                bq24296m::VbusStatus::AdapterPort => "Adapter Port",
-                bq24296m::VbusStatus::Otg => "OTG",
-            };
-
-            let charge_status = match status.get_charge_status() {
-                bq24296m::ChargeStatus::NotCharging => "Not Charging",
-                bq24296m::ChargeStatus::PreCharge => "Pre-Charge",
-                bq24296m::ChargeStatus::FastCharging => "Fast Charging",
-                bq24296m::ChargeStatus::ChargeDone => "Charge Done",
-            };
-
-            let charge_fault_status = match faults.get_charge_fault_status() {
-                bq24296m::ChargeFaultStatus::Normal => "Normal",
-                bq24296m::ChargeFaultStatus::InputFault => "Input Fault",
-                bq24296m::ChargeFaultStatus::ThermalShutdown => "Thermal Shutdown",
-                bq24296m::ChargeFaultStatus::ChargeTimerExpired => "Charge Timer Expired",
-            };
-
-            let ntc_fault_status = match faults.get_ntc_fault_status() {
-                bq24296m::NtcFaultStatus::Normal => "Normal",
-                bq24296m::NtcFaultStatus::Cold => "Cold",
-                bq24296m::NtcFaultStatus::Hot => "Hot",
-                bq24296m::NtcFaultStatus::ColdAndHot => "Cold and Hot",
-            };
-
-            PowerStatsResponse {
-                ok: true,
-                // Status register decoded values
-                vbus_status,
-                charge_status,
-                dpm_active: status.is_dpm_active(),
-                power_good: status.is_power_good(),
-                thermal_regulation_active: status.is_thermal_regulation_active(),
-                vsys_regulation_active: status.is_vsys_regulation_active(),
-                // Fault register decoded values
-                watchdog_fault: faults.is_watchdog_fault(),
-                otg_fault: faults.is_otg_fault(),
-                charge_fault_status,
-                battery_fault: faults.is_battery_fault(),
-                ntc_fault_status,
-                ntc_cold_fault: faults.is_ntc_cold_fault(),
-                ntc_hot_fault: faults.is_ntc_hot_fault(),
-                // Boost converter state
-                boost_converter_enabled: stats.boost_enabled,
-            }
-        }
-        _ => {
-            info!("Power stats not available yet");
-            return Err(BadRequest::Bad("Power stats not available yet".to_string()));
-        }
-    };
-
-    Ok(picoserve::response::Json(response))
+        tx.close(close_reason).await
+    }
 }
 
 /// Initialize and run the web server
