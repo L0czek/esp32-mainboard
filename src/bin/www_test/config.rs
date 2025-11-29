d1e1a1315dab1f4fc5231c451f881f7f74e2f482
krzys-h <krzys_h@interia.pl>
2025-11-29 17:59:47 +0100

    Make the wifi password configurable

--- src/bin/www_test/config.rs
+++ src/bin/www_test/config.rs
@@ -1,4 +1,10 @@
-pub static WIFI_SSID: &str = "internet.www_2.4GHz";
-pub static WIFI_PASSWORD: &str = "warsztatywww.pl";
-pub static AP_SSID: &str = "ESP32-AP";
-pub static AP_PASSWORD: &str = "password123";
+pub static WIFI_SSID: &str = env!("WIFI_SSID");
+pub static WIFI_PASSWORD: &str = env!("WIFI_PASSWORD");
+pub static AP_SSID: &str = match option_env!("AP_SSID") {
+    Some(val) => val,
+    None => "ESP32-AP",
+};
+pub static AP_PASSWORD: &str = match option_env!("AP_PASSWORD") {
+    Some(val) => val,
+    None => "password123",
+};
